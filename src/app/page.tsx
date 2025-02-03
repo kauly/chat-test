89b13b576b7197f10a48e1f71fd7e11ec577503f
kauly <kauly@hotmail.com>
2025-02-03 16:36:06 -0300

    fix: center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 export default function Home() {
   return (
     <div
-      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
+      className="w-full h-screen flex justify-center items-center"
       id="gpt-max-webchat"
     >
       <Script
