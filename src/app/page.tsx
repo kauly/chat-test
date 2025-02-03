c928d960cb9378937eb07955c80e5a1e9e52e7cc
kauly <kauly@hotmail.com>
2025-02-03 16:38:42 -0300

    fix: sdda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,14 @@ import Script from "next/script";
 
 export default function Home() {
   return (
-    <div
-      className="w-full h-screen flex justify-center items-center"
-      id="gpt-max-webchat"
-    >
-      <Script
-        defer
-        id="script-gptmax"
-        src="https://d1nfa9z59crrh.cloudfront.net/scripts-files/index.umd.js?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxNDIzNCwia25vd2xlZGdlX2Jhc2VfaWQiOjQyNDU3fQ.Xrf1Z7UBhX4u-Vz7S3GvDvoYJn1AxSf6flLCbhFiCeQ"
-      />
+    <div className="w-full h-screen flex justify-center items-center">
+      <div id="gpt-max-webchat" className="w-[800px] h-[600px] bg-red-500">
+        <Script
+          defer
+          id="script-gptmax"
+          src="https://d1nfa9z59crrh.cloudfront.net/scripts-files/index.umd.js?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxNDIzNCwia25vd2xlZGdlX2Jhc2VfaWQiOjQyNDU3fQ.Xrf1Z7UBhX4u-Vz7S3GvDvoYJn1AxSf6flLCbhFiCeQ"
+        />
+      </div>
     </div>
   );
 }
