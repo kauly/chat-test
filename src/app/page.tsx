668013cb9bfa68ecbbc1338e57f7d10786ee4378
kauly <kauly@hotmail.com>
2025-02-17 09:52:34 -0300

    feat: bg positivo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import Script from "next/script";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div id="gpt-max-webchat" className="w-[800px] h-[600px] bg-red-500">
+    <div className="w-full h-screen flex justify-center items-center  bg-positivo bg-cover">
+      <div id="gpt-max-webchat" className="w-[800px] h-[600px]">
         <Script
           defer
           id="script-gptmax"
-          src="https://d1nfa9z59crrh.cloudfront.net/scripts-files/index.umd.js?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxNDIzNCwia25vd2xlZGdlX2Jhc2VfaWQiOjQyNDU3fQ.Xrf1Z7UBhX4u-Vz7S3GvDvoYJn1AxSf6flLCbhFiCeQ"
+          src="https://d1nfa9z59crrh.cloudfront.net/scripts-files/index.umd.js?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxNDI4Niwia25vd2xlZGdlX2Jhc2VfaWQiOjQzODQ3fQ.X_GUyLzlamgT9mViBP9vDLarYwEJoXbgoOYJoKwTxUk"
         />
       </div>
     </div>
